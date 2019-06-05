0114d3973d38e13745eed5f2db9287e15dde1d14
andku <andreykushnarev83@gmail.com>
2019-06-05 04:44:23 +0300

    dynamical create emptyTemplate

--- LuckyTickets.php
+++ LuckyTickets.php
@@ -9,50 +9,6 @@ class LuckyTickets
     public $to;
     public $sumToOneDigit = true;
 
-    protected $emptyTemplate = [
-        0 => 0, //for ticket 000000
-        1 => 0,
-        2 => 0,
-        3 => 0,
-        4 => 0,
-        5 => 0,
-        6 => 0,
-        7 => 0,
-        8 => 0,
-        9 => 0,
-    ];
-
-    protected $emptyFullTemplate = [
-        0 => 0, //for ticket 000000
-        1 => 0,
-        2 => 0,
-        3 => 0,
-        4 => 0,
-        5 => 0,
-        6 => 0,
-        7 => 0,
-        8 => 0,
-        9 => 0,
-        10 => 0,
-        11 => 0,
-        12 => 0,
-        13 => 0,
-        14 => 0,
-        15 => 0,
-        16 => 0,
-        17 => 0,
-        18 => 0,
-        19 => 0,
-        20 => 0,
-        21 => 0,
-        22 => 0,
-        23 => 0,
-        24 => 0,
-        25 => 0,
-        26 => 0,
-        27 => 0,
-    ];
-
     /**
      * LuckyTickets constructor.
      * @param int $from
@@ -134,7 +90,7 @@ class LuckyTickets
      */
     protected function getEmptyTemplate()
     {
-        return $this->sumToOneDigit ? $this->emptyTemplate : $this->emptyFullTemplate;
+        return $this->sumToOneDigit ? array_fill_keys(range(0, 9), 0) : array_fill_keys(range(0, 9*3), 0);
     }
 
     protected $_fullThousand = null;
